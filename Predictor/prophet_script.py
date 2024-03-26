fff52685ae95cd252a9a72d5b75c419d01ad7507
xololt-dev <105434979+xololt-dev@users.noreply.github.com>
2024-03-26 10:36:30 +0100

    Added events for the model

--- Predictor/prophet_script.py
+++ Predictor/prophet_script.py
@@ -6,6 +6,7 @@ import sys
 import numpy as np
 from datetime import datetime
 
+# TODO: Automatic dates calculation or saving the adjusted model and using it later on
 def includeEvents(startDate, endDate, industry):
     events = None
 
@@ -24,11 +25,55 @@ def includeEvents(startDate, endDate, industry):
         events = pd.concat((events, lockdowns))
 
     match industry:
-    #case 'Ksiazki' | 'Books':
-    #case 'Chemia':
-    #case 'Spozywcze':
-    #case 'Zabawki':
-        case 'Biuro':
+        case 'Chemia':
+            wielkanoc = pd.DataFrame({
+                    'holiday': 'wielkanoc',
+                    'ds': pd.to_datetime(['2018-04-01', '2019-04-21', '2020-04-12', '2021-04-04',
+                                        '2022-04-17', '2023-04-09', '2024-03-31', '2025-04-20',]),
+                    'lower_window': -14,
+                    'upper_window': 0,
+                })
+            events = pd.concat((events, wielkanoc))
+
+            swieta = pd.DataFrame({
+                    'holiday': 'swieta',
+                    'ds': pd.to_datetime(['2018-12-25', '2019-12-25', '2020-12-25', '2021-12-25',
+                                        '2022-12-25', '2023-12-25', '2024-12-25', '2025-12-25',]),
+                    'lower_window': -14,
+                    'upper_window': 0,
+                })
+            events = pd.concat((events, swieta))
+
+        case 'Spozywcze':
+            post_swieta_nowy = pd.DataFrame({
+                    'holiday': 'post-swieta-nowy',
+                    'ds': pd.to_datetime(['2018-01-01', '2019-01-01', '2020-01-01', '2021-01-01',
+                                        '2022-01-01', '2023-01-01', '2024-01-01', '2025-01-01',]),
+                    'lower_window': 0,
+                    'upper_window': 14,
+                })
+            events = pd.concat((events, post_swieta_nowy))
+
+        case 'Zabawki':
+            dzien_dziecka = pd.DataFrame({
+                    'holiday': 'dzien-dziecka',
+                    'ds': pd.to_datetime(['2018-06-01', '2019-06-01', '2020-06-01', '2021-06-01',
+                                        '2022-06-01', '2023-06-01', '2024-06-01', '2025-06-01',]),
+                    'lower_window': -14,
+                    'upper_window': 0,
+                })
+            events = pd.concat((events, dzien_dziecka))
+
+            mikolajki = pd.DataFrame({
+                    'holiday': 'Mikolajki',
+                    'ds': pd.to_datetime(['2018-12-06', '2019-12-06', '2020-12-06', '2021-12-06',
+                                        '2022-12-06', '2023-12-06', '2024-12-06', '2025-12-06',]),
+                    'lower_window': -14,
+                    'upper_window': 0,
+                })
+            events = pd.concat((events, mikolajki))
+
+        case 'Biuro' | 'Ksiazki':
             szkola = pd.DataFrame({
                     'holiday': 'start-rok-szkolny',
                     'ds': pd.to_datetime(['2018-09-01', '2019-09-01', '2020-09-01', '2021-09-01',

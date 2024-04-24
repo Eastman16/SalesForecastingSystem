c142385ce329258b6a3022f9e957d38db9b6a7d4
Xololt <105434979+xololt-dev@users.noreply.github.com>
2024-04-24 14:49:45 +0200

    StatsForecast refactor with basic flags implementation

--- Predictor/statsforecast_script.py
+++ Predictor/statsforecast_script.py
@@ -2,6 +2,7 @@ import os
 import sys
 import numpy as np
 import pandas as pd
+from datetime import datetime
 
 # Plot
 import matplotlib.pyplot as plt
@@ -9,74 +10,139 @@ import plotly.graph_objects as go
 
 # StatsForecast
 from statsforecast import StatsForecast
+from statsforecast.utils import ConformalIntervals
 from statsforecast.models import (
     HoltWinters,
-    CrostonClassic as Croston,
-    HistoricAverage,
     SeasonalNaive,
-    AutoARIMA,
-    AutoETS,
+    AutoTheta,
+    AutoCES,
     DynamicOptimizedTheta as DOT,
 )
-from datasetsforecast.losses import rmse
+from datasetsforecast.losses import rmse, mae, smape
+
+# Our
+from utils import exportToFile
 
 def main(argv):
     fileName = argv[1]
+    
+    exportFileName = "Export_"+datetime.today().strftime('%Y-%m-%d_%H-%M-%S')
+    
+    predictPeriod = 365
+    # TODO: properly implement weeks and months
+    frequency = 'D'
+
+    export = False
+    plot = False
+    crossValidate = False
 
     # this makes it so that the outputs of the predict methods have the id as a column 
     # instead of as the index
     os.environ['NIXTLA_ID_AS_COL'] = '1'
     
+    state = None
+    for a in argv[1:]:
+        match a:
+            case '-e':
+                export = True
+                crossValidate = True
+                exportFileName = "Export_"+datetime.today().strftime('%Y-%m-%d_%H-%M-%S')
+                print("Exporting dataframe to ", exportFileName)
+            case '-f':
+                state = 'Frequency'
+                print(state, end=": ")
+            case '--help':
+                # help()
+                return 0
+            case '-p':
+                plot = True
+            case '-t':
+                state = 'Time'
+                print(state, end=": ")
+            case '-v':
+                crossValidate = True
+                print('Cross validation enabled')
+            case _:
+                match state:
+                    case 'Time':
+                        predictPeriod = int(a)
+                    case 'Frequency':
+                        frequency = a
+                    case _:
+                        fileName = a
+                state = None
+                print(a)
+
     try:
         df = pd.read_csv(fileName)
     except:
         print("ERROR: Can't open "+fileName+"!")
         return 1
     
-    df.head()
-    df['ds'] = pd.to_datetime(df['ds'])
-    
+    df['ds'] = pd.to_datetime(df['ds'], format="%Y-%m-%d")
+
     models = [
-        #AutoETS(season_length=365),
-        HoltWinters(),
-        Croston(),
+        AutoTheta(season_length=365),
+        AutoCES(season_length=365, alias='AutoCES'),
+        HoltWinters(season_length=365),
         SeasonalNaive(season_length=365),
-        # HistoricAverage(),
-        DOT(season_length=365),# decomposition_type='additive')
+        DOT(season_length=365),
     ]
+    modelNames = ['AutoTheta', 'AutoCES', 'HoltWinters', 'SeasonalNaive', 'DynamicOptimizedTheta']
 
     # Standard stats forecast
     sf = StatsForecast(
         df = df,
-        models=models, #[DOT(season_length = 12)],
-        freq='D',#'ME',
-        fallback_model = SeasonalNaive(season_length=365),
+        models=models,
+        freq=frequency,#'ME',
+        # fallback_model = SeasonalNaive(season_length=365),
         n_jobs=-1,
     )
 
-    cross_vali_df = sf.cross_validation(
-        df=df,
-        h=30,
-        step_size=30,
-        n_windows=12
-    )
+    rmses = []
+    maes = []
+    smapes = []
 
-    sf.fit(df)
+    if crossValidate == True:
+        cross_vali_df = sf.cross_validation(
+            df=df,
+            h=30,
+            step_size=30,
+            n_windows=12
+        )
 
-    forecast_df = sf.forecast(df=df, h=360, level=[90])
+        print(cross_vali_df)
 
-    plot1 = sf.plot(df, forecast_df, engine='plotly')
-    plot1.show()
+        for name in modelNames:
+            rmses.append(rmse(cross_vali_df['y'], cross_vali_df[name]))
+            maes.append(mae(cross_vali_df['y'], cross_vali_df[name]))
+            smapes.append(smape(cross_vali_df['y'], cross_vali_df[name]))
+
+            print("RMSE using cross-validation (" + name + "): ", rmses[-1])
+            print("MAE: ", maes[-1])
+            print("SMAPE: ", smapes[-1])
     
-    rmseLoss = rmse(cross_vali_df['actual'], cross_vali_df['HoltWinters'])
-    print("RMSE using cross-validation (HoltWinters): ", rmseLoss)
-    rmseLoss = rmse(cross_vali_df['actual'], cross_vali_df['CrostonClassic'])
-    print("RMSE using cross-validation (Croston): ", rmseLoss)
-    rmseLoss = rmse(cross_vali_df['actual'], cross_vali_df['SeasonalNaive'])
-    print("RMSE using cross-validation (SeasonalNaive): ", rmseLoss)
-    rmseLoss = rmse(cross_vali_df['actual'], cross_vali_df['DynamicOptimizedTheta'])
-    print("RMSE using cross-validation (DOT): ", rmseLoss)
+    sf.fit(df)
+    # sf.fit(df, prediction_intervals=ConformalIntervals(n_windows=12, h=30))
 
+    forecast_df = sf.forecast(df=df, h=predictPeriod, level=[90])
+
+    if plot == True:
+        plot1 = sf.plot(df, forecast_df, engine='plotly')
+        plot1.show()
+
+    if export == True:
+        tuning_results = pd.DataFrame(modelNames)
+        tuning_results['rmse'] = rmses
+        tuning_results['mae'] = maes
+        tuning_results['smape'] = smapes
+        tuning_results = tuning_results.sort_values(by=['rmse'])
+
+        tuning_results.to_csv(exportFileName + "_cv_stats.csv", index=False)
+
+        for name in modelNames:
+            exportToFile(cross_vali_df, df, name, exportFileName)
+    
     return 0
 
 if __name__ == '__main__':

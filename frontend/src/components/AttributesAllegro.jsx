35a74db3cca6fec67f046380482342bfb4060f30
Justyna02bu <127955513+Justyna02bu@users.noreply.github.com>
2024-06-11 16:31:09 +0200

    Łączenie z API Allegro

--- frontend/src/components/AttributesAllegro.jsx
+++ frontend/src/components/AttributesAllegro.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import SelectItem from "./SelectItem";
 import businessType from "../lists/businessType.json";
@@ -6,12 +6,20 @@ import country from "../lists/countryList.json";
 import predictionFrequency from "../lists/predictionFrequency.json";
 import Allegro from "../assets/allegro.png";
 import convertToNumericPeriod from "./Attributes";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import Slider from "@mui/material/Slider";
 import Typography from "@mui/material/Typography";
 
 function AttributesAllegro() {
   const navigate = useNavigate();
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const allegroCode = searchParams.get("code");
+
+  const CLIENT_ID = "3e70bfb322f446ab836e553d44ef807f"; //  CLIENT_ID z Allegro
+  const CLIENT_SECRET = "xOtaFv1BcdQ2dUkgmbZyaUCCdLT6f1JwsT1lfrIotzT5v0qmwjrk0hwzEztthd8x"; //  CLIENT_SECRET z Allegro
+  const REDIRECT_URI = "https://predykcjakpz.servehttp.com/import-allegro-sale"; // Twój REDIRECT_URI
+  const TOKEN_ENDPOINT = "https://allegro.pl/auth/oauth/token"; // Stały endpoint tokenu
 
   // Default options for dropdowns
   const defaultOptions = {
@@ -28,6 +36,94 @@ function AttributesAllegro() {
     setSelectedOptions(defaultOptions);
   }, []);
 
+  const fetchAccessToken = async (code) => {
+    try {
+      const response = await axios.post(
+        TOKEN_ENDPOINT,
+        new URLSearchParams({
+          grant_type: "authorization_code", // Stała wartość dla OAuth 2.0
+          code: code,
+          redirect_uri: REDIRECT_URI,
+          client_id: CLIENT_ID,
+          client_secret: CLIENT_SECRET,
+        }),
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+      const accessToken = response.data.access_token;
+      fetchAllegroSalesReport(accessToken);
+    } catch (error) {
+      console.error("Error fetching access token:", error);
+    }
+  };
+
+  const fetchAllegroSalesReport = async (token) => {
+    try {
+      const response = await axios.get(
+        "https://api.allegro.pl/sale/reports", // Zamień na poprawny endpoint Allegro
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            Accept: "application/vnd.allegro.public.v1+json",
+          },
+        }
+      );
+      const salesData = response.data;
+      console.log("Fetched Allegro sales data:", salesData);
+
+      // Przetwarzanie danych sprzedaży w wymagany format
+      const formattedSalesData = salesData.map((item) => {
+        const date = item.date;
+        const amount = item.amount;
+        return `${date},${amount.toFixed(3)}`;
+      });
+
+      console.log("Formatted sales data:", formattedSalesData.join("\n"));
+
+      // Wysyłanie sformatowanych danych do backendu
+      await sendSalesDataToBackend(formattedSalesData.join("\n"));
+    } catch (error) {
+      console.error("Error fetching Allegro sales report:", error);
+    }
+  };
+
+  const sendSalesDataToBackend = async (formattedSalesData) => {
+    try {
+      const response = await axios.post(
+        "http://192.168.195.63:5000/predict",
+        {
+          salesData: formattedSalesData,
+          country: selectedOptions.country,
+          industry: selectedOptions.businessType,
+          isRetail: selectedOptions.sunday === "Tak" ? true : false,
+          period: convertToNumericPeriod(
+            selectedOptions.predictionLength,
+            selectedOptions.predictionFrequency
+          ),
+          frequency:
+            selectedOptions.predictionFrequency === "Dzienna"
+              ? "D"
+              : selectedOptions.predictionFrequency === "Tygodniowa"
+              ? "W"
+              : "M",
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log("Server response:", response.data);
+      navigate("/output-sales");
+    } catch (error) {
+      console.error("Error sending sales data to backend:", error);
+    }
+  };
+
   const handleDropdownChange = (name, value) => {
     setSelectedOptions({
       ...selectedOptions,
@@ -42,65 +138,15 @@ function AttributesAllegro() {
     });
   };
 
-  const fileInputRef = useRef(null);
-
-  // Function to handle file change
-  const handleFileChange = async (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      const fileName = file.name;
-      const extension = fileName.split(".").pop();
-      if (["xlsx", "txt", "csv"].includes(extension)) {
-        console.log("Selected file:", file);
-        try {
-          const formData = new FormData();
-          formData.append("file", file);
-          formData.append("country", selectedOptions.country);
-          formData.append("industry", selectedOptions.businessType);
-          formData.append(
-            "isRetail",
-            selectedOptions.sunday === "Tak" ? true : false
-          );
-          const numericPeriod = convertToNumericPeriod(
-            selectedOptions.predictionLength,
-            selectedOptions.predictionFrequency
-          );
-          formData.append("period", numericPeriod);
-
-          const frequencyChar =
-            selectedOptions.predictionFrequency === "Dzienna"
-              ? "D"
-              : selectedOptions.predictionFrequency === "Tygodniowa"
-              ? "W"
-              : "M";
-          formData.append("frequency", frequencyChar);
-
-          // Sending POST request to server
-          const response = await axios.post(
-            "http://192.168.195.63:5000/predict",
-            formData,
-            {
-              headers: {
-                "Content-Type": "multipart/form-data",
-              },
-            }
-          );
-
-          console.log("Server response:", response.data);
-          navigate("/output-sales");
-        } catch (error) {
-          console.error("Error uploading file:", error);
-        }
-      } else {
-        alert("Please select a file in .xlsx, .csv, or .txt format");
-      }
+  const handleAllegroClick = () => {
+    if (allegroCode) {
+      fetchAccessToken(allegroCode);
+    } else {
+      alert("Allegro code not found");
     }
   };
 
-  const [isFileClickedAllegro, setFileClickedAllegro] = useState(false);
-
   return (
-    // Displaying the entire list of attributes needed for the operation of the AI model, individual attributes are displayed in SelectItem
     <div>
       <div className="top-[40x] h-[60px]">
         <SelectItem
@@ -140,28 +186,11 @@ function AttributesAllegro() {
         ></Slider>
       </div>
       <div className="flex justify-center mt-[30px]">
-        <input
-          type="file"
-          accept=".xlsx, .txt"
-          ref={fileInputRef}
-          className="hidden"
-          onChange={handleFileChange}
-        />
         <button
-          className={`py-2 bg-ifirma-orange text-black font-bold rounded-full transition duration-150 ease-in-out transform w-[220px] flex justify-center items-center ${
-            isFileClickedAllegro
-              ? "scale-90 opacity-75"
-              : "hover:bg-ifirma-orange-darker hover:scale-105 active:scale-95"
-          }`}
-          onClick={() => {
-            setFileClickedAllegro(true);
-            fileInputRef.current.click();
-            setTimeout(() => {
-              setFileClickedAllegro(false);
-            }, 150);
-          }}
+          className="py-2 bg-ifirma-orange text-black font-bold rounded-full transition duration-150 ease-in-out transform w-[220px] flex justify-center items-center hover:bg-ifirma-orange-darker hover:scale-105 active:scale-95"
+          onClick={handleAllegroClick}
         >
-          <div>Wczytaj plik z</div>
+          <div>Wczytaj dane z</div>
           <img src={Allegro} alt="Logo" className="ml-1 w-[70px] h-auto" />
         </button>
       </div>
--- frontend/src/components/ImportAllegroSales.jsx
+++ frontend/src/components/ImportAllegroSales.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
-import Attributes from "./AttributesAllegro";
+import React from "react";
+import AttributesAllegro from "./AttributesAllegro"; 
 
 const ImportAllegroSales = () => {
   return (
-    // Displaying attributes and buttons related to Allegro
     <>
       <div className="flex justify-center h-screen">
         <div className="bg-white rounded-lg relative flex flex-col items-center pt-3 w-[450px] h-[470px] mt-[50px]">
@@ -12,7 +11,7 @@ const ImportAllegroSales = () => {
           </div>
           <div className="mt-[20px]">
             <div className="top-[40x] h-[70px]">
-              <Attributes />
+              <AttributesAllegro /> 
             </div>
           </div>
         </div>
